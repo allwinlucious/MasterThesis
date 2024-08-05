d6068f9b2d180c5d8980643c4fa4812f4bd6d613
allwin <allwin.lucious@neura-robotics.com>
2024-08-05 12:04:45 +0200

    analytical 99%, ML1 40% ML2 5% added jacobain into sota, changes in dockerfile for torchview, added ML data validation.csv

---
+++ utils/plotcfg.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+from matplotlib_inline.backend_inline import set_matplotlib_formats
+import warnings
+warnings.filterwarnings('ignore')
+set_matplotlib_formats('pdf', 'png')
+plt.rcParams['savefig.dpi'] = 75
+
+plt.rcParams['figure.autolayout'] = False
+plt.rcParams['figure.figsize'] = 10, 6
+plt.rcParams['axes.labelsize'] = 18
+plt.rcParams['axes.titlesize'] = 20
+plt.rcParams['font.size'] = 16
+plt.rcParams['lines.linewidth'] = 2.0
+plt.rcParams['lines.markersize'] = 8
+plt.rcParams['legend.fontsize'] = 14
+
+plt.rcParams['text.usetex'] = True
+plt.rcParams['font.family'] = "serif"
+plt.rcParams['font.serif'] = "cm"
